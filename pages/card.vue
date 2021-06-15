3fe5e3bdc38a59d9b1861535cb65fd376a04a815
pkshefi <pkshefi@gmail.com>
2021-06-16 00:41:54 +0530

    card sample

---
+++ pages/card.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="p-4 ">
+    
+    <div class="max-w-xs w-auto rounded-lg overflow-hidden shadow-xl mb-4 ">
+      <img
+        class="w-full"
+        src="https://cdn-products.eneba.com/resized-products/TNVA57HERJBpx6HwHU5cxpXnp0upLCUEXFUBdYSyfvA_350x200_2x-0.jpeg"
+        alt="Farcry 6 releases soon"
+      >
+      <div class="px-6 py-4 pb-6 bg-gray-900">
+        <div class="mb-2 text-gray-600 -m-px">Trailer
+          <span class="text-yellow-200">NEW</span>
+        </div>
+        <div class="font-bold text-gray-400 mb-2">FarCry 6</div>
+        <span class="mb-2 text-gray-600 -m-px">Release at 12/09/21</span>
+      </div>
+     
+    </div>
+    <NuxtLink to="./wide-card">
+      <TButton>Button</TButton>
+    </NuxtLink>
+  </div>
+
+
+
+
+  
+</template>
+
+<style lang="postcss" scoped>
+.badge {
+  @apply inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700;
+  &:hover {
+    @apply bg-gray-300;
+  }
+}
+</style>
---
+++ pages/wide-card.vue
@@ -0,0 +1,34 @@
+<template>
+<div class=" bg-gray-900 p-2 w-80 max-w-3xl sm:w-full sm:p-4 h-full rounded-2xl shadow-lg flex flex-col sm:flex-row gap-5 select-none">
+    <div
+        style='background: url("https://cdn-products.eneba.com/resized-products/TNVA57HERJBpx6HwHU5cxpXnp0upLCUEXFUBdYSyfvA_350x200_2x-0.jpeg")'
+        class="h-64 w-2/5 rounded-xl bg-gray-100 bg-center bg-cover"
+    >
+    </div>
+
+    
+    <div class="">
+       <ul class="flex sm:flex-1 flex-col gap-2 p-1">
+            <li>.</li>
+            <li>.</li>
+            <li>.</li>
+        </ul>
+        <h1 class="text-lg sm:text-xl font-semibold  text-gray-600">
+            Black Widow
+        </h1>
+       
+    </div>
+    <div >
+        <span class="text-yellow-200">Movie</span>
+        <span class="text-yellow-200">Initial Release</span>
+    </div>
+
+
+
+
+        <NuxtLink to="./card">
+            <TButton>Back</TButton>
+        </NuxtLink>
+</div>
+
+</template>
